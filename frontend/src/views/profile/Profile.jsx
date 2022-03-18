112a6de0cfabaded1655f9a16dc6efe1a3e805ed
Redempto D. Legaspi III <redempto.legaspi@sun-asterisk.com>
2022-03-18 16:16:25 +0800

    [SELS-TASK][OPTIMIZE] Minor Syntax Changes #2

--- frontend/src/views/profile/Profile.jsx
+++ frontend/src/views/profile/Profile.jsx
@@ -45,64 +45,68 @@ const Profile = ({ view = "" }) => {
 
     }
 
+    const displayPage = () => {
+        return (
+            user != null ?
+                (<div className="w-full mt-5">
+                    <Avatar className="w-8/12 block m-auto" />
+                    <div className="block align-top pt-2 text-center">
+                        <Link to={`/users/${id}/activity`}>
+                            <span className="text-black block font-bold text-xl mt-5">
+                                {user != null && user.full_name}
+                            </span>
+                        </Link>
+                        <div className='w-9/12 m-auto'>
+                            <Divider />
+                        </div>
+                        <div className="mt-3 flex w-8/12 m-auto">
+                            <div className="w-1/2">
+                                {user.followers.length}
+                                <span className="block">followers</span>
+                            </div>
+                            <div className="w-1/2">
+                                {user.following.length}
+                                <span className="block">following</span>
+                            </div>
+                        </div>
+                        {
+                            parseInt(id) !== currentUser.current.id &&
+                            <button className="btn btn-primary mt-8 w-7/12" disabled={isFollowing == null || isDisabled} onClick={() => handleFollow()}>
+                                {isFollowing ? "Unfollow" : "Follow"}
+                            </button>
+                        }
+                        <Link to={`/users/${id}/history`}><span className="text-blue-700 block mt-5">Learned 20 words</span></Link>
+                    </div>
+                </div>) :
+                (<div className="w-full mt-5 h-1/2 text-2xl flex justify-center">
+                    Loading...
+                </div>)
+        )
+    }
+
     useEffect(() => {
         currentUser.current != null &&
-        UserService.getUser(id)
-            .then((response) => {
-                setUser(response.data.user)
-                const followData = response.data.user.followers.find((user) => user.id === currentUser.current.id)
-                if (typeof followData === 'undefined' || followData === null) {
-                    setIsFollowing(false)
-                }
-                else {
-                    setIsFollowing(true)
-                }
-                setIsDisabled(false)
-            });
+            UserService.getUser(id)
+                .then((response) => {
+                    setUser(response.data.user)
+                    const followData = response.data.user.followers.find((user) => user.id === currentUser.current.id)
+                    if (typeof followData === 'undefined' || followData === null) {
+                        setIsFollowing(false)
+                    }
+                    else {
+                        setIsFollowing(true)
+                    }
+                    setIsDisabled(false)
+                });
     }, [id])
 
     return (
         <div className="text-black flex-1 w-10/12 m-auto">
             {
-                currentUser.current != null && 
+                currentUser.current != null &&
                 <>
                     <div className="inline-block align-top w-4/12 p-10">
-                        {
-                            user == null ?
-                                <div className="w-full mt-5 h-1/2 text-2xl flex justify-center">
-                                    Loading...
-                                </div> :
-                                <div className="w-full mt-5">
-                                    <Avatar className="w-8/12 block m-auto" />
-                                    <div className="block align-top pt-2 text-center">
-                                        <Link to={`/users/${id}/activity`}>
-                                            <span className="text-black block font-bold text-xl mt-5">
-                                                {user != null && user.full_name}
-                                            </span>
-                                        </Link>
-                                        <div className='w-9/12 m-auto'>
-                                            <Divider />
-                                        </div>
-                                        <div className="mt-3 flex w-8/12 m-auto">
-                                            <div className="w-1/2">
-                                                {user.followers.length}
-                                                <span className="block">followers</span>
-                                            </div>
-                                            <div className="w-1/2">
-                                                {user.following.length}
-                                                <span className="block">following</span>
-                                            </div>
-                                        </div>
-                                        {
-                                            parseInt(id) !== currentUser.current.id &&
-                                            <button className="btn btn-primary mt-8 w-7/12" disabled={isFollowing == null || isDisabled} onClick={() => handleFollow()}>
-                                                {isFollowing ? "Unfollow" : "Follow"}
-                                            </button>
-                                        }
-                                        <Link to={`/users/${id}/history`}><span className="text-blue-700 block mt-5">Learned 20 words</span></Link>
-                                    </div>
-                                </div>
-                        }
+                        {displayPage()}
                     </div>
                     {display}
                 </>
